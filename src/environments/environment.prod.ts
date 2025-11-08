1f880744090df0c6c990b97804fdd18787015e73
maertin <mutuverria00@gmail.com>
2025-11-08 19:56:01 -0300

    arregla ruta de api deployada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://site--mercado-sinergico-backend--ky7qwjgxzhrc.code.run/api/'
+  apiUrl: 'https://site--mercado-sinergico-backend--ky7qwjgxzhrc.code.run/api'
 };
