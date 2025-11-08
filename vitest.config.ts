01031b12a65c2bdac4cf78daf8cb9bb4a08c5eec
maertin <mutuverria00@gmail.com>
2025-11-08 19:19:25 -0300

    actualiza configuración de test y elimina algunos

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vitest/config';
 import angular from '@analogjs/vite-plugin-angular';
+import path from 'path';
 
 export default defineConfig({
     plugins: [angular()],
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: ['src/test/setup.ts'],  // ← Cambiá esta línea
+        setupFiles: ['src/test/setup.ts'],
         include: ['**/*.spec.ts'],
     },
-    resolve: {
+     resolve: {
         alias: {
-            '@app': '/src/app',
+            '@app': path.resolve(__dirname, './src/app'),
+            '@environments': path.resolve(__dirname, './src/environments')
         }
     }
 });
